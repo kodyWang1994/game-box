63ce267e1449b0c2a2926dffb154a80940a0bf5f
KodyWang <kodywang@dingtalk.com>
2021-02-15 21:42:28 +0800

    refine home style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,12 +9,13 @@
       <a href="/draw" class="menu-item">画板</a> -->
       <a href="index.html#/snake" class="menu-item">贪吃蛇</a>
       <a href="index.html#/eliminate" class="menu-item">消消乐</a>
+      <div @click="gotoChineseChess" class="menu-item">中国象棋</div>
+      <a href="index.html#/spider-card" class="menu-item">蜘蛛卡牌</a>
       <a href="index.html#/pop-block" class="menu-item">消灭方块</a>
       <a href="index.html#/drag-block" class="menu-item">拖动方块</a>
       <a href="index.html#/color" class="menu-item">烧脑颜色</a>
       <a href="index.html#/picture" class="menu-item">拼图</a>
       <a href="index.html#/number-eliminate" class="menu-item">数字消消乐</a>
-      <a href="index.html#/spider-card" class="menu-item">蜘蛛卡牌</a>
       <a href="index.html#/draw" class="menu-item">画板</a>
       <a href="index.html#/date" class="menu-item">日历</a>
     </div>
@@ -30,6 +31,11 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+    }
+  },
+  methods: {
+    gotoChineseChess () {
+      window.location.href = 'https://kodywang1994.github.io/chinese-chess/dist/index.html'
     }
   }
 }
@@ -56,7 +62,7 @@ export default {
 
 .abort-us a {
   text-decoration-line: none;
-  color: #333;
+  color: #fff;
   text-shadow: #7777d8 2px 2px 10px;
 }
 
@@ -65,24 +71,30 @@ export default {
   font-size: 28px;
   margin: 75px 0;
   color: #fff;
-  text-shadow: #7777d8 2px 2px 10px;
+  text-shadow: #333 4px 4px 4px;
 }
 
 .menu-wrap {
-  width: 60%;
+  width: 80%;
   margin: auto;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-around;
 }
 
 .menu-item {
+  width: 35%;
   display: block;
   text-decoration-line: none;
-  border-radius: 25px;
+  border-radius: 5px;
   background-color: #7777d8;
-  height: 40px;
-  line-height: 40px;
-  margin-bottom: 15px;
+  height: 50px;
+  line-height: 50px;
+  margin-bottom: 25px;
   text-align: center;
   color: #fff;
-  box-shadow: #7777d9 0 0 6px 0px;
+  box-shadow: #333 1px 6px 10px 1px;
+  font-size: 14px;
 }
 </style>
